c5b82a87e5a690a962acd085155d3571e6da977d
MaxMasayesva <masayesvam@gmail.com>
2021-11-21 19:22:28 -0600

    Create commentsOutline.py

---
+++ commentsOutline.py
@@ -0,0 +1,24 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+#thisisasinglelinecomment
+
+
+#2) Make a multi-line comment.
+"""
+this
+is
+a
+multi
+line
+comment
+"""
+
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+var= 2 #this is a single line comment
+
