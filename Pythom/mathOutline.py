fbee9b248a2c38a68e1b665a4f0cb06d56dc392f
MaxMasayesva <masayesvam@gmail.com>
2022-02-02 21:15:26 -0600

    mathOutline

---
+++ Pythom/mathOutline.py
@@ -0,0 +1,64 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+x= "Max" + "Masayesva"
+print(x)
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+var1=10  var2=10
+x= "var1" + "var2"
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+var1=2   var2=3
+x= "var1" - "var2"
+
+
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+z=2  y=4
+x= "z" * "y"
+
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+x= "good" * "Morning"
+
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+f= 5  g=7
+x= "f" / "g"
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+float(7)
+x= "float" / "float"
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+h=6  u=8
+x= "h" % "u"
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+n= 3   c= 9
+x= "n" // "c"
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+Var3= 9   Var4= 6
+x= "Var3" ** "Var4"
+#12) Make any type of operation with a common SYNTAX error
+x= "Max" =+Masayesva"
+pint(x
+
---
+++ Pythom/variablesOutline.py
@@ -0,0 +1,75 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list)
+#integer
+a=5
+a=4
+
+#float
+
+a=3.45
+a=4.25
+
+#string
+
+a= "max"
+
+#boolean
+
+a=True
+
+a=False
+
+#list
+
+a=[1, 2, 3, 4]
+
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+
+VarA= 1
+Vara= 2
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+var=4
+var= "string"
+
+#4)
+#Make a variable and set it to a variable you have already created:
+varTwo=var
+
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+
+2= [1, 2, 3]
+
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first
+#character in that string:
+
+m= "max"
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+varTwo= var
+print(varTwo)
+
+
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+Var a= "max
+"
+
