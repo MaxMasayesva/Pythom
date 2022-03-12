7283fa879351b70bcae415dd995015bce0aafcbd
MaxMasayesva <masayesvam@gmail.com>
2022-03-11 22:29:04 -0600

    Create conditionalOutline.py

---
+++ conditionalOutline.py
@@ -0,0 +1,52 @@
+'''
+This outline will help solidify concepts from the Conditionals lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+from _ast import If
+from setuptools.command.build_ext import if_dl
+
+#1) Make a int variable named a. Now make an if statement, and if a is more than
+#5, print out "a is more than 5."
+= If
+a = 9
+ if a>5:
+    print("a is more than 5")
+
+
+
+#2) Make a boolean variable named b. Now make an if statement, and if b is True
+#print out "b is True."
+= If
+b = "I like dogs
+x = True
+if x:
+    print("b is true")
+
+
+#3) Make an int variable named c. Now make an if/elif statement, and if c is 
+#more than 0, print "c is positive". Else if c is less than 0, print "c is
+#negative".
+= if
+b = "I like dogs"
+x = True
+if x:
+    print("b is true")
+    
+
+
+#4) Make an int variable named d. Now make an if/elif/else statement, and if 
+#d is more than 0, print "d is positive". Else if d is less than 0, print "c is
+#negative". Else, print "d is zero".
+= if, else
+c = -1
+if c > 0
+print("c is positive"
+
+
+
+#5) Make an if statement with any comparison, with a common SYNTAX error.
+= If
+x= 6
+if s > 2
+print ("more than 2")
+
